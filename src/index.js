6d715dedfa860eebc985f8c3b83983c2315db6f7
liulirui <admin@liulirui.com>
2022-11-01 23:06:54 +0800

    feat:use hooks

--- src/index.js
+++ src/index.js
@@ -1,164 +1,16 @@
-import React from 'react';
+import React, {memo, useEffect, useMemo, useRef, useState} from 'react';
 import {
   StyleSheet,
-  View,
-  Text,
-  TextInput,
+  Animated,
   Dimensions,
   Keyboard,
-  Animated,
+  TextInput,
   TouchableOpacity,
+  View,
+  Text,
 } from 'react-native';
 
 const windowHeight = Dimensions.get('window').height;
-
-
-export default class extends React.Component {
-  static defaultProps = {
-    show: false,
-    title: '',
-    onCancel: () => {},
-    onSubmit: text => {},
-    cancelText: 'cancel',
-    submitText: 'submit',
-    cancelStyle: {},
-    submitStyle: {},
-    style: {},
-    inputStyle: {},
-    placeholder: '',
-    colors: {},
-    num: 1,
-    content: [],
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: [],
-      keyboardHeight: new Animated.Value(windowHeight),
-    };
-    this.cancelF = () => this.props.onCancel();
-  }
-  componentWillMount() {
-    this.keyboardDidShowListener = Keyboard.addListener(
-      'keyboardDidShow',
-      e => {
-        Animated.timing(this.state.keyboardHeight, {
-          toValue: windowHeight - e.endCoordinates.height,
-          duration: 150,
-          useNativeDriver: false,
-        }).start();
-      },
-    );
-    this.keyboardDidHideListener = Keyboard.addListener(
-      'keyboardDidHide',
-      () => {
-        Animated.timing(this.state.keyboardHeight, {
-          toValue: windowHeight,
-          duration: 150,
-          useNativeDriver: false,
-        }).start();
-      },
-    );
-  }
-  componentWillUnmount() {
-    this.keyboardDidShowListener.remove();
-    this.keyboardDidHideListener.remove();
-  }
-  render() {
-    const content = this.state.content;
-    const myContent = [];
-    const num = this.props.num;
-    if (!this.props.content || this.props.content.length !== num) {
-      for (let i = 0; i < num; i++) {
-        myContent.push(
-          <TextInput
-            key={'input_' + i}
-            style={[
-              styles.input,
-              {
-                backgroundColor: this.props.colors.background,
-                color: this.props.colors.text,
-                borderColor: this.props.colors.border,
-              },
-              this.props.inputStyle,
-            ]}
-            defaultValue={content[i] || ''}
-            placeholderTextColor={this.props.colors.placeholder}
-            autoFocus={i === 0}
-            onChangeText={text => {
-              content[i] = text;
-              this.setState({content: content});
-            }}
-            placeholder={this.props.placeholder}
-            underlineColorAndroid={'transparent'}
-          />,
-        );
-      }
-    } else {
-      for (let i = 0; i < num; i++) {
-        myContent.push(this.props.content[i]);
-      }
-    }
-    return this.props.show ? (
-      <Animated.View
-        style={[
-          styles.animated_container,
-          {height: this.state.keyboardHeight},
-        ]}>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={styles.masker}
-          onPress={this.cancelF}
-        />
-        <View
-          style={[
-            styles.container,
-            {backgroundColor: this.props.colors.card},
-            this.props.style,
-          ]}>
-          {this.props.title ? (
-            typeof this.props.title === 'string' ? (
-              <Text style={styles.title}>{this.props.title}</Text>
-            ) : (
-              this.props.title
-            )
-          ) : null}
-          {myContent}
-          <View
-            style={[
-              styles.btn_container,
-              {borderColor: this.props.colors.border},
-              this.props.btnsStyle,
-            ]}>
-            <Text
-              style={[
-                styles.btn,
-                {color: this.props.colors.text},
-                this.props.cancelStyle,
-              ]}
-              onPress={this.cancelF}>
-              {this.props.cancelText}
-            </Text>
-            <Text
-              style={[
-                styles.btn,
-                {color: this.props.colors.text},
-                this.props.submitStyle,
-              ]}
-              onPress={() => {
-                this.setState({content: []});
-                this.props.onSubmit(
-                  num === 1 ? this.state.content[0] : this.state.content,
-                );
-              }}>
-              {this.props.submitText}
-            </Text>
-          </View>
-        </View>
-      </Animated.View>
-    ) : null;
-  }
-}
 
 const styles = StyleSheet.create({
   masker: {
@@ -167,7 +19,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0,0,0,.6)',
+    backgroundColor: 'rgba(0,0,0,0.6)',
   },
   animated_container: {
     position: 'absolute',
@@ -218,3 +70,126 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
   },
 });
+
+const PickerActionSheet = props => {
+  const {
+    show = false,
+    title = '',
+    onCancel,
+    onSubmit,
+    cancelText = 'cancel',
+    submitText = 'submit',
+    cancelStyle,
+    submitStyle,
+    btnsStyle,
+    style,
+    inputStyle,
+    placeholder = '',
+    colors,
+    num = 1,
+    content = [],
+  } = props;
+
+  const keyboardHeight = useRef(new Animated.Value(windowHeight));
+  const [inputValue, setInputValue] = useState([]);
+
+  const InputContent = useMemo(() => {
+    const myContent = [];
+    if (!content || content.length !== num) {
+      for (let i = 0; i < num; i++) {
+        myContent.push(
+            <TextInput
+                key={'input_' + i}
+                style={[
+                  styles.input,
+                  {
+                    backgroundColor: colors.background,
+                    color: colors.text,
+                    borderColor: colors.border,
+                  },
+                  inputStyle,
+                ]}
+                placeholderTextColor={colors.placeholder}
+                autoFocus={i === 0}
+                onChangeText={text => {
+                  setInputValue(prev => {
+                    prev[i] = text;
+                    return prev;
+                  });
+                }}
+                placeholder={placeholder}
+            />,
+        );
+      }
+    } else {
+      for (let i = 0; i < num; i++) {
+        myContent.push(content[i]);
+      }
+    }
+    return myContent;
+  }, [colors, content, inputStyle, num, placeholder]);
+
+  useEffect(() => {
+    const keyboardDidShow = Keyboard.addListener('keyboardDidShow', e => {
+      Animated.timing(keyboardHeight.current, {
+        toValue: windowHeight - e.endCoordinates.height,
+        duration: 150,
+        useNativeDriver: false,
+      }).start();
+    });
+    const keyboardDidHide = Keyboard.addListener('keyboardDidHide', () => {
+      Animated.timing(keyboardHeight.current, {
+        toValue: windowHeight,
+        duration: 150,
+        useNativeDriver: false,
+      }).start();
+    });
+    return () => {
+      keyboardDidShow.remove();
+      keyboardDidHide.remove();
+    };
+  }, []);
+
+  return show ? (
+      <Animated.View
+          style={[styles.animated_container, {height: keyboardHeight}]}>
+        <TouchableOpacity
+            activeOpacity={1}
+            style={styles.masker}
+            onPress={onCancel}
+        />
+        <View style={[styles.container, {backgroundColor: colors.card}, style]}>
+          {title ? (
+              typeof title === 'string' ? (
+                  <Text style={styles.title}>{title}</Text>
+              ) : (
+                  title
+              )
+          ) : null}
+          {InputContent}
+          <View
+              style={[
+                styles.btn_container,
+                {borderColor: colors.border},
+                btnsStyle,
+              ]}>
+            <Text
+                style={[styles.btn, {color: colors.text}, cancelStyle]}
+                onPress={onCancel}>
+              {cancelText}
+            </Text>
+            <Text
+                style={[styles.btn, {color: colors.text}, submitStyle]}
+                onPress={() => {
+                  setInputValue([]);
+                  onSubmit?.(num === 1 ? inputValue[0] : inputValue);
+                }}>
+              {submitText}
+            </Text>
+          </View>
+        </View>
+      </Animated.View>
+  ) : null;
+};
+
+export default memo(PickerActionSheet);
